218e7a10b34aa0714952b75b93038edc8d176e29
vitalDamNs <DamNs2209915686@outlook.com>
2026-03-17 15:13:29 +0800

    成功导入了俄罗斯方块的环境，并且测试成功Torch的GPU版本

--- GameEnv.py
+++ GameEnv.py
@@ -1,3 +1,11 @@
 import gymnasium as gym
-import ale_py
+import torch
 
+'''
+This file is used to test the environment, and make sure it can be rendered properly.
+'''
+
+# env=gym.make('ALE/Tetris-v5', render_mode='human')
+print(torch.__version__)
+print(torch.version.cuda)
+print(torch.cuda.is_available())
