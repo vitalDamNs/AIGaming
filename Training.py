83fde01d88aac3281a62b7340039e5b2ffe8df57
vitalDamNs <DamNs2209915686@outlook.com>
2026-03-22 14:45:23 +0800

    修改了一些错误

--- Training.py
+++ Training.py
@@ -4,7 +4,7 @@ import ale_py
 '''
 这个文件是训练模块，是训练的主要部分
 '''
-Train_env=gym.make('ALE/Teris-v5', render_mode="human")
+Train_env=gym.make('ALE/Tetris-v5', render_mode="human")
 print('训练开始')
 train_model=Learning(Train_env,algorithm='DQN',verbose=1,total_timesteps=100000,cnnPolicy=False,device='cuda',Game_name='Tetris')
 train_model.learn()
